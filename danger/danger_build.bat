19050762e8d29738701b8d01cce8c1f68140db34
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-05-18 01:13:35 -0400

    drafting NES decompressor (standard)

---
+++ danger/danger_build.bat
@@ -0,0 +1,35 @@
+@del output\*.o
+@del output\*.nes
+@del output\*.dbg
+@del output\*.map
+
+
+cc65\bin\ca65 -o output\huffmunch.o -g ..\huffmunch.s
+@IF ERRORLEVEL 1 GOTO error
+
+REM cc65\bin\ca65 -o output\huffmunch_canonical.o -g ..\huffmunch_canonical.s
+@IF ERRORLEVEL 1 GOTO error
+
+REM cc65\bin\ca65 -o output\danger.o -g ..\danger.s
+@IF ERRORLEVEL 1 GOTO error
+
+REM cc65\bin\ca65 -o output\danger_canonical.o -D CANONICAL -g ..\danger.s
+@IF ERRORLEVEL 1 GOTO error
+
+REM cc65\bin\ld65 -o output\danger.nes -m output\danger.map --dbgfile output\danger.dbg -C danger.cfg output\danger.o output\huffmunch.o
+@IF ERRORLEVEL 1 GOTO error
+
+REM cc65\bin\ld65 -o output\danger_canonical.nes -m output\danger_canonical.map --dbgfile output\danger_canonical.dbg -C danger.cfg output\danger_canonical.o output\huffmunch_canonical.o
+@IF ERRORLEVEL 1 GOTO error
+
+@echo.
+@echo.
+@echo Build successful!
+@pause
+@GOTO end
+:error
+@echo.
+@echo.
+@echo Build error!
+@pause
+:end
---
+++ huffmunch.s
@@ -0,0 +1,248 @@
+; Huffmunch
+; Brad Smith, 2019
+; https://github.com/bbbradsmith/huffmunch
+
+.importzp huffmunch_zpblock
+
+.export huffmunch_load ; in: Y:X = index, hm_node = pointer to data block
+.export huffmunch_read ; reads 1 byte from stream, result in A (flags unreliable)
+
+hm_node   = <(huffmunch_zpblock + 0); pointer to current node of tree
+hm_stream = <(huffmunch_zpblock + 2) ; pointer to bitstream
+hm_tree   = <(huffmunch_zpblock + 4) ; pointer to tree base
+hm_byte   = <(huffmunch_zpblock + 6) ; current byte of bitstream
+hm_status = <(huffmunch_zpblock + 7) ; bits 0-2 = bits left in hm_byte, bit 7 = string with suffix
+hm_length = <(huffmunch_zpblock + 8) ; bytes left in current string
+.assert (huffmunch_zpblock + 9) <= 256, error, "huffmunch_zpblock requires 9 bytes on zero page"
+
+.segment "CODE"
+
+.proc huffmunch_load
+	; hm_node = header
+	; Y:X = index
+	hm_temp = hm_byte ; temporary 16-bit value in hm_status:hm_byte
+	; hm_stream = (index * 2)
+	sty hm_stream+1
+	txa
+	asl
+	sta hm_stream+0
+	rol hm_stream+1
+	; hm_temp = stream count * 2
+	ldy #1
+	lda (hm_node), Y
+	sta hm_temp+1
+	dey
+	lda (hm_node), Y
+	asl
+	sta hm_temp+0
+	rol hm_temp+1
+	; hm_node = header + 2
+	lda hm_node+0
+	clc
+	adc #2
+	sta hm_node+0
+	bcc :+
+		inc hm_node+1
+	:
+	; hm_tree = header + 2 + (4 * stream count) [ready]
+	lda hm_node+0
+	clc
+	adc hm_temp+0
+	sta hm_tree+0
+	lda hm_node+1
+	adc hm_temp+1
+	sta hm_tree+1
+	lda hm_tree+0
+	clc
+	adc hm_temp+0
+	sta hm_tree+0
+	lda hm_tree+1
+	adc hm_temp+1
+	sta hm_tree+1
+	; hm_node = header + 2 + (index * 2)
+	lda hm_node+0
+	clc
+	adc hm_stream+0
+	sta hm_node+0
+	lda hm_node+1
+	adc hm_stream+1
+	sta hm_node+1
+	; hm_stream = stream address [ready]
+	; Y = 0
+	lda (hm_node), Y
+	sta hm_stream+0
+	iny
+	lda (hm_node), Y
+	sta hm_stream+1
+	; hm_node = header + 2 + (index * 2) + (2 * stream count)
+	lda hm_node+0
+	clc
+	adc hm_temp+0
+	sta hm_node+0
+	lda hm_node+1
+	adc hm_temp+1
+	sta hm_node+1
+	; hm_node = stream length [ready]
+	; Y = 1
+	lda (hm_node), Y
+	tax
+	dey
+	lda (hm_node), Y
+	sta hm_node+0
+	stx hm_node+1
+	; initialize other data
+	lda #0
+	sta hm_byte ; doesn't strictly need to be initialized
+	sta hm_status
+	sta hm_length
+	rts
+.endproc
+
+.proc huffmunch_read
+	ldy #0
+	lda hm_length ; string bytes pending
+	beq string_empty
+emit_byte:
+	dec hm_length
+	lda (hm_node), Y
+	inc hm_node+0
+	bne :+
+		inc hm_node+1
+	:
+	rts
+string_empty:
+	; hm_length = 0
+	; Y = 0
+	bit hm_status
+	bpl walk_tree
+	; follow suffix
+	lda (hm_node), Y
+	clc
+	adc hm_tree+0
+	tax
+	iny
+	lda (hm_node), Y
+	adc hm_tree+1
+	sta hm_node+1
+	stx hm_node+0
+	dey ; ldy #0
+	lda (hm_node), Y
+	iny
+	cmp #2
+	beq leaf2
+	tax
+	lda hm_status
+	and #$7F ; clear high bit, no more suffix
+	sta hm_status
+	cpx #1
+	beq leaf1
+	; 0 is the only other valid value
+leaf0:
+	; hm_length = 0
+	; hm_status has high bit clear
+	; (next read will walk_tree)
+	; Y = 1
+	lda (hm_node), Y
+	rts
+leaf1:
+	; hm_status high bit is clear (no suffix)
+leaf2:
+	; hm_status high bit is set (suffix) if leaf2
+	; Y = 1
+	lda (hm_node), Y
+	sta hm_length ; length must be > 0
+	lda hm_node+0
+	clc
+	adc #2
+	sta hm_node+0
+	bcc :+
+		inc hm_node+1
+	:
+	ldy #0
+	jmp emit_byte
+walk_tree:
+	; hm_length = 0
+	; Y = 0
+	lda hm_tree+0 ; return to head of tree
+	sta hm_node+0
+	lda hm_tree+1
+	sta hm_node+1
+walk_node:
+	; Y = 0
+	lda (hm_node), Y
+	cmp #3
+	bcs node3
+	iny
+	cmp #2
+	beq node2
+	cmp #1
+	beq node1
+node0:
+	; Y = 1
+	jmp leaf0
+node1 = leaf1
+node2:
+	; Y = 1
+	lda hm_status
+	ora #$80 ; set suffix flag
+	sta hm_status
+	jmp leaf2
+node3:
+	; Y = 0
+	tax ; x = 3-255
+	; read bit
+	lda hm_status
+	bne :+
+		lda #8
+		sta hm_status
+		lda (hm_stream), Y
+		sta hm_byte
+		inc hm_stream+0
+		bne :+
+		inc hm_stream+1
+	:
+	dec hm_status
+	lsr hm_byte
+	bcs walk_right
+walk_left:
+	cpx #255
+	beq walk_left_long
+	inc hm_node+0
+	bne :+
+		inc hm_node+1
+	:
+	jmp walk_node
+walk_left_long:
+	lda hm_node+0
+	clc
+	adc #3
+	sta hm_node+0
+	bcc :+
+		inc hm_node+1
+	:
+	jmp walk_node
+walk_right:
+	cpx #255
+	beq walk_right_long
+	txa
+	clc
+	adc hm_node+0
+	sta hm_node+0
+	bcc :+
+		inc hm_node+1
+	:
+	jmp walk_node
+walk_right_long:
+	iny ; Y = 1
+	lda (hm_node), Y
+	clc
+	adc hm_node+0
+	tax
+	iny ; Y = 2
+	lda (hm_node), Y
+	adc hm_node+1
+	sta hm_node+1
+	stx hm_node+0
+	ldy #0
+	jmp walk_node
+.endproc
